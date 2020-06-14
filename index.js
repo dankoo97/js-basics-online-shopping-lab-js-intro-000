289dafcda453be236cef2f8b166551d30f6b2672
Daniel A Koontz <dak.7997@gmail.com>
2020-06-14 21:09:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function removeFromCart(item) {
 
   for (let i = 0; i < cart.length; i ++) {
     if (item === cart[i].itemName) {
-      cart.splice(i)
+      cart.splice(i,1)
       return cart
     }
   }
