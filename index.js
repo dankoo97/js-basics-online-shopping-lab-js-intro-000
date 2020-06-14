a6c4b535ac7466611b0e979001ea0d664d7872c5
Daniel A Koontz <dak.7997@gmail.com>
2020-06-14 21:16:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === 'undefined') {
+  if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   }
   var totalPrice = total()
